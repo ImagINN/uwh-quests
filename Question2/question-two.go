2f6110c230a05c2005ea594f81c0caff30b0d131
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-10-29 21:11:03 +0300

    Refactor RecursiveFactorial function and add example usage

--- Question2/question-two.go
+++ Question2/question-two.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	if nb == 0 || nb == 1 {
+		return 1
+	}
+	return nb * RecursiveFactorial(nb-1)
+}
+
 func main() {
-	// Add your code here
+	number := 6
+	factorial := RecursiveFactorial(number)
+	fmt.Printf("%d! = %d\n", number, factorial)
 }
