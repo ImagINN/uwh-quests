540fa3ae6d374c634e5089daf80c8a7db43bc1c0
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-10-30 16:22:08 +0300

    Add FirstRune function to return the first rune of a string

--- Quest4/Question1/firstRune.go
+++ Quest4/Question1/firstRune.go
@@ -1,5 +1,21 @@
+/*
+Instructions:
+Write a function that returns the first rune of a string.
+*/
 package main
 
-func main() {
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
+func FirstRune(s string) rune {
+	r, _ := utf8.DecodeRuneInString(s)
+	return r
+}
+
+func main() {
+	value := "Hello!"
+	firstRune := FirstRune(value)
+	fmt.Printf("First rune of %s is %c\n", value, firstRune)
 }
